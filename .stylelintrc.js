53c360168bae982bd42ef82afc620bcf0707b852
Богдан <zvyagintsevbi@pik.ru>
2020-05-09 03:42:54 +0300

    feat: add stylelint
    adds:
    - stylelint
    - stylelint-order
    - stylelint-scss
    - AirBnB stylelint config
    - Idiomatic order stylelint config
    - Prettier
    - stylelint-prettier
    - Prettier stylelint config
    - Husky
    - lint-staged
    - VS Code settings

---
+++ .stylelintrc.js
@@ -0,0 +1,6 @@
+module.exports = {
+  extends: ["stylelint-config-airbnb", "stylelint-config-idiomatic-order", "stylelint-prettier/recommended"],
+  plugins: ["stylelint-order", "stylelint-scss"],
+  rules: {
+  }
+};
